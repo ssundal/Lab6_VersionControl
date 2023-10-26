7c0aafa748684fdf7cf2213fb31c9c1d34cf48d5
shansundal <csshan3@gmail.com>
2023-10-25 20:54:34 -0400

    Final with encode function

--- main.py
+++ main.py
@@ -9,14 +9,44 @@ Description:
 
 # THIS IS WHERE SHAN WRITE THE ENCODE FUNCTION
 
-def encoder():
+def encoder(password):
+    user_password = str(password)
+    res = []
+    for i in user_password:
+        new_number = str(int(i) + 3)
+        res.append(new_number)
+    return "".join(res)
+
+
+def decode(password):
     pass
 
 
 def main():
-    user_input = input("Enter password: ")
+    continue_program = True
+    while continue_program:
+        encoded_password = ""
+        print("Menu\n"
+              "-------------\n"
+              "1. Encode\n"
+              "2. Decode\n"
+              "3. Quit\n")
+
+        user_input = int(input("Please enter an option: "))
+
+        if user_input == 1:
+            get_password = input("Please enter your password to encode: ")
+            encoded_password = encoder(get_password)
+            print("Your password has been encoded and stored!")
+            print()
+
+        if user_input == 2:
+            print(f"The encoded password is {encoder(get_password)}, and the original password is {decode(encoded_password)}.")
+            print()
+
+        if user_input == 3:
+            continue_program = False
 
 
 if __name__ == "__main__":
     main()
-ghdskufhgdsk
