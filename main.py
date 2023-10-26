85c6f828f440f484d8969f6f73def60ef4fff2dc
arins <asareddy12345@gmail.com>
2023-10-25 21:07:45 -0400

    final

--- main.py
+++ main.py
@@ -18,10 +18,18 @@ def encoder(password):
     return "".join(res)
 
 
-def decode(password):
-    pass
+# code written by arin
+# iterates through string and subtracts 3 for every iteration
+def decoder(encoded_password):
+    encoded_password = str(encoded_password)
+    string_result = ''
+    for i in encoded_password:
+        result = int(i) - 3
+        string_result += str(result)
+    return string_result
 
 
+# displays menu and asks user what they want to do
 def main():
     continue_program = True
     while continue_program:
@@ -33,17 +41,17 @@ def main():
               "3. Quit\n")
 
         user_input = int(input("Please enter an option: "))
-
+        # encodes password and stores
         if user_input == 1:
             get_password = input("Please enter your password to encode: ")
             encoded_password = encoder(get_password)
             print("Your password has been encoded and stored!")
             print()
-
+        # decodes password and prints both encoded password and decoded password
         if user_input == 2:
-            print(f"The encoded password is {encoder(get_password)}, and the original password is {decode(encoded_password)}.")
+            print(f"The encoded password is {encoder(get_password)}, and the original password is {decoder(encoder(get_password))}.")
             print()
-
+        # exits program
         if user_input == 3:
             continue_program = False
 
