d42c2826309eca1619094d54b3728db56ab42bd0
shansundal <csshan3@gmail.com>
2023-10-25 20:30:42 -0400

    bla

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Lab 5
+Authors: Shan Sundal and Arin Sareddy
+Class: COP3502C
+Section: 22282
+Description:
+"""
+
+
+def encoder():
+    pass
+
+
+def main():
+    user_input = input("Enter password: ")
+
+
+if __name__ == "__main__":
+    main()
